67b9b7d63693d7e01a2b00956eb6c134a425bb47
SharonFashola1 <15221657-SharonFashola1@users.noreply.replit.com>
2023-09-17 22:41:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hello! Welcome to [name_of_site].\nI am chatbot here to offer my assistance.\n")
+user_name=input("Please enter your username: ")
+user_age=input("Please enter your age: ")
+
+
